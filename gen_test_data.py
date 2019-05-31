2e6a84c058c6022b211197f4ed1bc914459aa10c
Jingyang Sui <jingyang@uber.com>
2019-05-31 09:56:04 -0700

    first pull request

--- gen_test_data.py
+++ gen_test_data.py
@@ -25,20 +25,25 @@ schema = {
 def genRandStr(letters="abc", len=3):
     return "".join(random.choice(letters) for i in range(len))
 
+def padZero(s, length=2):
+    return '0'*(length-len(s)) + s
+
 def genDate(precision="d", startYear=2010):
     dateStr = ""
-    if precision in ["y", "M", "d", "h", "m", "s"]:
+    if precision in ["y", "M", "d", "h", "m", "s", "ms"]:
         dateStr = str(startYear + random.randint(0, 5))
-    if precision in ["M", "d", "h", "m", "s"]:
-        dateStr = dateStr + "-" + str(random.randint(1, 12))
-    if precision in ["d", "h", "m", "s"]:
-        dateStr = dateStr + "-" + str(random.randint(1, 28))
-    if precision in ["h", "m", "s"]:
-        dateStr = dateStr + " " + str(random.randint(0, 23))
-    if precision in ["m", "s"]:
-        dateStr = dateStr + ":" + str(random.randint(0, 59))
-    if precision in ["s"]:
-        dateStr = dateStr + ":" + str(random.randint(0, 59))
+    if precision in ["M", "d", "h", "m", "s", "ms"]:
+        dateStr = dateStr + "-" + padZero(str(random.randint(1, 12)))
+    if precision in ["d", "h", "m", "s", "ms"]:
+        dateStr = dateStr + "-" + padZero(str(random.randint(1, 28)))
+    if precision in ["h", "m", "s", "ms"]:
+        dateStr = dateStr + "T" + padZero(str(random.randint(0, 23)))
+    if precision in ["m", "s", "ms"]:
+        dateStr = dateStr + ":" + padZero(str(random.randint(0, 59)))
+    if precision in ["s", "ms"]:
+        dateStr = dateStr + ":" + padZero(str(random.randint(0, 59)))
+    if precision in ["ms"]:
+        dateStr = dateStr + ":" + padZero(str(random.randint(0, 999)), 3)
     return dateStr
 
 
@@ -49,7 +54,7 @@ def insertData():
         colC = colA + " " + genRandStr() + " " + genRandStr()
         colD = random.randint(0, 20)
         colE = random.uniform(0, 20)
-        date = genDate("s")
+        date = genDate('s')
 
         payload = {"colA": colA, "colB": colB, "colC": colC, "colD": colD, "colE": colE, "date": date}
         resp = requests.post(url + postDataRoute, data=json.dumps(payload), headers=headers)
